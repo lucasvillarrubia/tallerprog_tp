99daa340466bc3400566772ffebf60f0eef25380
Lucas Villarrubia <105749679+lucasvillarrubia@users.noreply.github.com>
2024-11-02 21:01:22 -0300

    Delete build directory

--- build/CMakeFiles/taller_client.dir/client/character.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/taller_client.dir/client/character.cpp.o: \
- /home/lucas/CLionProjects/tallerprog_tp/client/character.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas/CLionProjects/tallerprog_tp/client/character.h \
- /home/lucas/CLionProjects/tallerprog_tp/client/coordinates.h
--- build/CMakeFiles/taller_common.dir/common/foo.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/taller_common.dir/common/foo.cpp.o: \
- /home/lucas/CLionProjects/tallerprog_tp/common/foo.cpp \
- /usr/include/stdc-predef.h \
- /home/lucas/CLionProjects/tallerprog_tp/./common/foo.h
